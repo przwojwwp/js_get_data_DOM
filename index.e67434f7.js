8ef66d2f144fae5a6b37c0b6ab02c910223d8470
Wojciech Przyłuski <przyluskiwojciech@gmail.com>
2024-02-26 11:19:46 +0100

    make build

---
+++ index.e67434f7.js
@@ -0,0 +1,4 @@
+// write your code here
+var t=document.querySelectorAll(".population"),e=0;t.forEach(function(t){var o=parseFloat(t.textContent.replace(/,/g,""));e+=o});var o=e/10;document.getElementsByClassName("total-population")[0].textContent=e.toLocaleString("en-US"),document.getElementsByClassName("average-population")[0].textContent=o.toLocaleString("en-US");//# sourceMappingURL=index.e67434f7.js.map
+
+//# sourceMappingURL=index.e67434f7.js.map
